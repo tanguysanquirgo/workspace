16a6779abf5082ea8870a6d30d1c4277d8a51cbb
casualsoty <sanquirgotanguy@gmail.com>
2025-02-22 14:08:05 +0100

    feat(workspace): init

---
+++ .workspace/index.ts
@@ -0,0 +1,3 @@
+import { installShellAliases } from './shell-aliases';
+
+installShellAliases();
---
+++ .workspace/shell-aliases/index.ts
@@ -0,0 +1,9 @@
+import { installBashAliases } from './bash-aliases';
+
+export const installShellAliases = () => {
+  const shell = process.env.COMSPEC || process.env.SHELL;
+
+  installBashAliases(shell);
+
+  console.log('\x1b[31mUnsupported shell detected\x1b[0m\n');
+};
